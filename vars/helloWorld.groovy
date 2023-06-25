373aa57036799d74eb6e039fb43d2ccdc580b5ea
shish990 <118693510+shish990@users.noreply.github.com>
2023-06-25 12:48:59 -0700

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
 def call(String name, String dayOfWeek) {
-  sh 'echo my name is ${name} From DevOps and today is ${dayOfWeek}'
+  sh "echo my name is ${name} From DevOps and today is ${dayOfWeek}"
 }
